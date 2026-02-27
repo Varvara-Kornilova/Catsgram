9d8630ae6826b6415cddebd3935e51388f81e56e
Varvara Kornilova <vrvrmoren@yandex.ru>
2026-02-27 17:58:55 +0300

    #добавить работу с User: Dal, Dao, Mapper etc

--- src/main/java/ru/yandex/practicum/catsgram/controller/PostController.java
+++ src/main/java/ru/yandex/practicum/catsgram/controller/PostController.java
@@ -3,57 +3,31 @@ package ru.yandex.practicum.catsgram.controller;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
-import ru.yandex.practicum.catsgram.exception.ParameterNotValidException;
-import ru.yandex.practicum.catsgram.model.Post;
-import ru.yandex.practicum.catsgram.model.SortOrder;
+import ru.yandex.practicum.catsgram.dto.NewPostRequest;
+import ru.yandex.practicum.catsgram.dto.PostDto;
+import ru.yandex.practicum.catsgram.dto.UpdatePostRequest;
 import ru.yandex.practicum.catsgram.service.PostService;
 
-import java.util.Collection;
-import java.util.Optional;
-
 @RestController
+@RequiredArgsConstructor
 @RequestMapping("/posts")
 public class PostController {
     private final PostService postService;
 
-    public PostController(PostService postService) {
-        this.postService = postService;
-    }
-
-    @GetMapping
-    public Collection<Post> findAll(@RequestParam(defaultValue = "desc") String sort,
-                                    @RequestParam(defaultValue = "0") int from,
-                                    @RequestParam(defaultValue = "10") int size) {
-        SortOrder sortOrder = SortOrder.from(sort);
-
-        if (sortOrder == null) {
-            throw new ParameterNotValidException("sort", "Некорректное значение сортировки. Допустимые значения: asc, desc");
-        }
-
-        if (from < 0) {
-            throw new ParameterNotValidException("from", "Некорректный отчет постов. Точка отсчета должна быть больше нуля");
-        }
-
-        if (size <= 0) {
-            throw new ParameterNotValidException("size", "Некорректный размер выборки. Размер должен быть больше нуля");
-        }
-
-        return postService.findAll(sortOrder, from, size);
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public PostDto createPost(@RequestBody NewPostRequest post) {
+        return postService.createPost(post);
     }
 
     @GetMapping("/{postId}")
-    public Optional<Post> findById(@PathVariable long postId) {
-        return postService.findById(postId);
+    @ResponseStatus(HttpStatus.OK)
+    public PostDto getPostById(@PathVariable("postId") long postId) {
+        return postService.getPostById(postId);
     }
 
-//    @PostMapping
-//    @ResponseStatus(HttpStatus.CREATED)
-//    public Post create(@RequestBody Post post) {
-//        return postService.create(post);
-//    }
-
-    @PutMapping
-    public Post update(@RequestBody Post newPost) {
-        return postService.update(newPost);
+    @PutMapping("/{postId}")
+    public PostDto updatePost(@PathVariable("postId") long postId, @RequestBody UpdatePostRequest post) {
+        return postService.updatePost(postId, post);
     }
 }
